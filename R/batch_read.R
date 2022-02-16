9d76c2c643c5bc11fd50aa276f783669092694f6
Chris Wolfe <wolfeclw@icloud.com>
2022-02-15 22:11:57 -0500

    GPS time_correction

--- R/batch_read.R
+++ R/batch_read.R
@@ -26,6 +26,8 @@
 #' column to denote sample ID. Default is NULL.
 #' @param sample_id user defined string to denote sample ID. If assigned, a
 #' value must also be supplied to `sample_col`. Default is NULL.
+#' @param time_correction numeric; second threshold to correct `Date_Time` using GPS 
+#' recorded time. 
 #' @return a tibble. Additional columns are created for 'Sampling_Event' and
 #' 'Sampling_Day.'  Sampling events represent discreet intervals in sampling
 #' defined by the `event_threshold`.  Sampling day is derived from the date
@@ -44,33 +46,79 @@
 ufp_batch_read <- function(paths, event_threshold = 10, tz = "America/New_York",
                            truncate_ufp = TRUE, coords = TRUE, ufp_check = FALSE,
                            participant_id = NULL, sample_col = NULL,
-                           sample_id = NULL) {
+                           sample_id = NULL, time_correction = NULL) {
   d_pufp <- map(paths, ~ ufp_read(.,
-    tz = tz, truncate_ufp = truncate_ufp,
-    coords = coords, ufp_check = ufp_check,
-    participant_id = participant_id,
-    sample_col = sample_col, sample_id = sample_id
+                                  tz = tz, truncate_ufp = truncate_ufp,
+                                  coords = coords, ufp_check = ufp_check,
+                                  participant_id = participant_id,
+                                  sample_col = sample_col, sample_id = sample_id
   )) %>%
-    reduce(., rbind) %>%
+    reduce(., bind_rows) %>%
     arrange(Date_Time) %>%
     mutate(
       lag_time = lag(Date_Time),
       Time_Diff.sec = abs(Date_Time - lag_time),
       break_yn = ifelse(is.na(Time_Diff.sec), 0,
-        ifelse(Time_Diff.sec > event_threshold * 60, 1, 0)
+                        ifelse(Time_Diff.sec > event_threshold * 60, 1, 0)
       ),
       Sampling_Event = cumsum(break_yn) + 1,
       Sampling_Day = as.numeric(Date - first(Date) + 1)
     ) %>%
     select(Date_Time:Time, Sampling_Day, Sampling_Event, everything()) %>%
     select(-c(lag_time, Time_Diff.sec, break_yn))
-
+  
   if (!is.null(sample_col)) {
     d_pufp <- relocate(d_pufp, {{ sample_col }})
   }
   if (!is.null(participant_id)) {
     d_pufp <- relocate(d_pufp, ID)
   }
+  
+  if(!is.null(time_correction) & !is.numeric(time_correction)) {
+    stop('`time correction` must assigned a numeric value.', call. = FALSE)
+  } else if (!is.null(time_correction)) {
+    d_gps_split <- d_pufp %>% 
+      group_split(Sampling_Event) 
+    
+    d_gps_time <- d_gps_split %>% 
+      map(., ~filter(., !is.na(lon))) %>% 
+      map_if(., ~nrow(.) > 0, ~mutate(.,
+                                      utc = lubridate::as_datetime(Date_Time, tz = 'UTC'),
+                                      gps_split = str_split(GPS_Signal, ','),
+                                      gps_time = map_chr(gps_split, 2),
+                                      gps_hr = substr(gps_time, 1, 2),
+                                      gps_min = substr(gps_time, 3, 4),
+                                      gps_sec = substr(gps_time, 5, 6),
+                                      gps_hms = hms::as_hms(str_c(gps_hr, gps_min, gps_sec, sep = ':')),
+                                      date_utc = lubridate::as_date(utc),
+                                      gps_dt = lubridate::as_datetime(paste(date_utc, gps_hms)),
+                                      gps_date_time = lubridate::force_tzs(gps_dt, tzone = 'UTC', tzone_out  = 'America/New_York'),
+                                      gps_diff = difftime(Date_Time, gps_date_time, units = 'min'))) 
+    
+    med_diff <- map(d_gps_time, ~median(.$gps_diff))
 
+    c_function <- function(l_event, tdiff) {
+
+      if (is.null(tdiff)) {
+        message(crayon::red(paste0('Sampling Event ', l_event$Sampling_Event[1],
+                                   ': Time correction not possible - no GPS data was recorded.')))
+        l_event
+      } else if (abs(tdiff) > time_correction) {
+        message(crayon::red(paste0('Sampling Event ', l_event$Sampling_Event[1], ': The median time difference (',
+                                   round(tdiff, digits = 1), ' mins) between the GPS signal and the PUFP reading was greater than the correction threshold (',
+                                   time_correction, ' mins). `Date_Time` has been corrected.')))
+        l_event$Date_Time <- l_event$Date_Time - tdiff
+
+        l_event
+      } else if (abs(tdiff) < time_correction) {
+        message(crayon::green(paste0('Sampling Event ', l_event$Sampling_Event[1], ': The median time difference (',
+                                     round(tdiff, digits = 1), ' mins) between the GPS signal and the PUFP reading was less than the correction threshold (',
+                                     time_correction, ' mins). `Date_Time` was not corrected.')))
+        l_event
+      }
+    }
+    d_pufp <- map2_df(d_gps_split, med_diff, ~c_function(.x, .y))
+  }
   d_pufp
+  
 }
--- R/read.R
+++ R/read.R
@@ -20,6 +20,9 @@
 #' column to denote sample ID. Default is NULL.
 #' @param sample_id user defined string to denote sample ID. If assigned, a
 #' value must also be supplied to `sample_col`. Default is NULL.
+#' @param time_correction numeric; second threshold to correct `Date_Time` using GPS 
+#' recorded time. 
+#' 
 #' @return a tibble.
 #' @export
 #'
@@ -37,11 +40,38 @@
 
 ufp_read <- function(path, tz = "America/New_York", truncate_ufp = TRUE, coords = TRUE,
                      ufp_check = FALSE, participant_id = NULL, sample_col = NULL,
-                     sample_id = NULL) {
+                     sample_id = NULL, time_correction = NULL) {
   pufp_df <- tag_pufp(path,
-    tz = tz, truncate_ufp = truncate_ufp, coords = coords,
-    ufp_check = ufp_check, participant_id = participant_id,
-    sample_col = sample_col, sample_id = sample_id
+                      tz = tz, truncate_ufp = truncate_ufp, coords = coords,
+                      ufp_check = ufp_check, participant_id = participant_id,
+                      sample_col = sample_col, sample_id = sample_id
   )
+  
+  if(!is.null(time_correction) & sum(pufp_df$GPS_Valid) == 0) {
+    message('Time correction not possible--no GPS data was recorded.')
+  } else if (!is.null(time_correction)) {
+    d_gps_time <- pufp_df %>% 
+      mutate(utc = lubridate::as_datetime(Date_Time, tz = 'UTC'),
+             gps_split = str_split(GPS_Signal, ','),
+             gps_time = map_chr(gps_split, 2),
+             gps_hr = substr(gps_time, 1, 2),
+             gps_min = substr(gps_time, 3, 4),
+             gps_sec = substr(gps_time, 5, 6),
+             gps_hms = hms::as_hms(str_c(gps_hr, gps_min, gps_sec, sep = ':')),
+             date_utc = lubridate::as_date(utc),
+             gps_dt = lubridate::as_datetime(paste(date_utc, gps_hms)),
+             gps_date_time = lubridate::force_tzs(gps_dt, tzone = 'UTC', tzone_out  = 'America/New_York'),
+             gps_diff = difftime(Date_Time, gps_date_time, units = 'min')) 
+    
+    med_diff <- median(d_gps_time$gps_diff)
+    
+    if (med_diff > time_correction) {
+      message(crayon::green(paste0('The mean time difference (',
+                                   round(med_diff, digits = 1), ' mins) between the GPS signal and the PUFP reading was greater than the correction threshold (',
+                                   time_correction, ' mins). `Date_Time` has been corrected.')))
+      pufp_df$Date_Time <- pufp_df$Date_Time - med_diff
+    }
+  }
+  
   pufp_df
 }
